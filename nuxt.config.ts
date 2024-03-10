2427019536411ed14171ee68289363dc18c8d702
cfabrice <fab@wonderweb.ch>
2024-03-10 15:58:18 +0100

    bon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/image"],
-  // following config is not working on netlify
-  image: {
-    provider: "ipx",
+  // following config is not working on netlify or vercel
+  // empty config for image is working out of the box for netlify and vercel
+
+ // image: {
+   // provider: "ipx",
     // ipx: {
     //   domains: ['https://test-nuxt-image.netlify.app'], // Replace with your Netlify domain
     //   // Your IPX configuration options
     // },
-  },
+  //},
 })
